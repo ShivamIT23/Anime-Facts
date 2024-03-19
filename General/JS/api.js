8112d03d78079ce146dc5fb42b765685fe13ea93
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-03-19 11:23:47 +0530

    Removing Axios Files

--- General/JS/api.js
+++ General/JS/api.js
@@ -2,7 +2,6 @@ let image = [];
 export const img = async ()=>{
   const options = {
     method: 'GET',
-    url: 'https://any-anime.p.rapidapi.com/v1/anime/gif/1',
     headers: {
       'X-RapidAPI-Key': '310d631612msh130c3edc31be461p1cfca5jsn1b52efd86862',
       'X-RapidAPI-Host': 'any-anime.p.rapidapi.com'
@@ -10,8 +9,9 @@ export const img = async ()=>{
   };
   
   try {
-    const response = await axios.request(options);
-    image = response.data.images;
+    const request = await fetch('https://any-anime.p.rapidapi.com/v1/anime/gif/1',options);
+    const response = request.json();
+    image = response.images;
     return image;
   } catch (error) {
     console.error(error);
